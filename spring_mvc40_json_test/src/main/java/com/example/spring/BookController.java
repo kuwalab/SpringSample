540eb332343e479d377a480cdcde6f28abc68eaa
kuwalab <arata.shinichi@gmail.com>
2014-02-02 17:38:11 +0900

    @Controllerを@RestControllerに変更

--- spring_mvc40_json_test/src/main/java/com/example/spring/BookController.java
+++ spring_mvc40_json_test/src/main/java/com/example/spring/BookController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,13 +14,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.databind.JsonMappingException.Reference;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
-@Controller
+@RestController
 public class BookController {
 	private static List<Book> bookList;
 
@@ -33,20 +33,19 @@ public class BookController {
 	}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index() {
-		return "book/index";
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView("book/index");
+		return mav;
 	}
 
 	@RequestMapping(value = "/books", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE
 			+ ";charset=utf-8")
-	@ResponseBody
 	public List<Book> books() {
 		return bookList;
 	}
 
 	@RequestMapping(value = "/books", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE
 			+ ";charset=utf-8")
-	@ResponseBody
 	public Book insert(@RequestBody @Valid Book book, Errors errors) {
 		for (ObjectError oe : errors.getAllErrors()) {
 			System.out.println(oe.getDefaultMessage());
@@ -57,7 +56,6 @@ public class BookController {
 	}
 
 	@ExceptionHandler(HttpMessageNotReadableException.class)
-	@ResponseBody
 	public Book handleException(HttpMessageNotReadableException ex,
 			WebRequest request) {
 		Throwable t = ex.getCause();
@@ -78,7 +76,6 @@ public class BookController {
 
 	@RequestMapping(value = "/books/{bookId}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE
 			+ ";charset=utf-8")
-	@ResponseBody
 	public Book update(@PathVariable("bookId") String initBookId,
 			@RequestBody Book book) {
 		for (int i = 0; i < bookList.size(); i++) {
@@ -96,7 +93,6 @@ public class BookController {
 
 	@RequestMapping(value = "/books/{bookId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE
 			+ ";charset=utf-8")
-	@ResponseBody
 	public Book delete(@PathVariable String bookId) {
 		for (int i = 0; i < bookList.size(); i++) {
 			Book book = bookList.get(i);
