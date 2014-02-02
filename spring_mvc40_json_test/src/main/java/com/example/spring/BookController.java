9e4e1cc9e55ed05ebec272fd71bbaacb768cc134
kuwalab <arata.shinichi@gmail.com>
2014-02-02 15:13:41 +0900

    Spring MVC 4.0へそのまま動作させて確認

---
+++ spring_mvc40_json_test/src/main/java/com/example/spring/BookController.java
@@ -0,0 +1,112 @@
+package com.example.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+@Controller
+public class BookController {
+	private static List<Book> bookList;
+
+	static {
+		bookList = new ArrayList<>();
+		bookList.add(new Book("1", "よくわかるSpring", 3000));
+		bookList.add(new Book("2", "よくわかるJava", 3200));
+		bookList.add(new Book("3", "よくわかるJUnit", 2800));
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		return "book/index";
+	}
+
+	@RequestMapping(value = "/books", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE
+			+ ";charset=utf-8")
+	@ResponseBody
+	public List<Book> books() {
+		return bookList;
+	}
+
+	@RequestMapping(value = "/books", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE
+			+ ";charset=utf-8")
+	@ResponseBody
+	public Book insert(@RequestBody @Valid Book book, Errors errors) {
+		for (ObjectError oe : errors.getAllErrors()) {
+			System.out.println(oe.getDefaultMessage());
+		}
+
+		bookList.add(book);
+		return book;
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	@ResponseBody
+	public Book handleException(HttpMessageNotReadableException ex,
+			WebRequest request) {
+		Throwable t = ex.getCause();
+		if (t != null && t instanceof InvalidFormatException) {
+			InvalidFormatException ife = (InvalidFormatException) t;
+			// エラーのフィールド。
+			for (Reference r : ife.getPath()) {
+				System.out.println(r.getFieldName());
+			}
+			// エラーになったフィールドの型
+			System.out.println("type= " + ife.getTargetType().getName());
+			// エラーになったフィールドの値
+			System.out.println("value=" + ife.getValue());
+		}
+
+		return new Book();
+	}
+
+	@RequestMapping(value = "/books/{bookId}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE
+			+ ";charset=utf-8")
+	@ResponseBody
+	public Book update(@PathVariable("bookId") String initBookId,
+			@RequestBody Book book) {
+		for (int i = 0; i < bookList.size(); i++) {
+			Book currentBook = bookList.get(i);
+			if (currentBook.getBookId().equals(initBookId)) {
+				currentBook.setBookId(book.getBookId());
+				currentBook.setBookName(book.getBookName());
+				currentBook.setPrice(book.getPrice());
+				return currentBook;
+			}
+		}
+
+		return new Book();
+	}
+
+	@RequestMapping(value = "/books/{bookId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE
+			+ ";charset=utf-8")
+	@ResponseBody
+	public Book delete(@PathVariable String bookId) {
+		for (int i = 0; i < bookList.size(); i++) {
+			Book book = bookList.get(i);
+			if (book.getBookId().equals(bookId)) {
+				bookList.remove(i);
+				return book;
+			}
+		}
+
+		return new Book();
+	}
+
+}
