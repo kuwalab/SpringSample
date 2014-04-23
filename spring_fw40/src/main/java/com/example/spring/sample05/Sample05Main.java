a7c5b7f497c1ece8abd73a990861d90f51b3ec9e
kuwalab <arata.shinichi@gmail.com>
2014-04-23 18:37:06 +0900

    spring_fw40にsample05を追加

---
+++ spring_fw40/src/main/java/com/example/spring/sample05/Sample05Main.java
@@ -0,0 +1,19 @@
+package com.example.spring.sample05;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Sample05Main {
+
+	/**
+	 * http://www.techscore.com/tech/Java/Others/Spring/3/
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"/sample05.xml");
+		SampleBean sampleBean = (SampleBean) ctx.getBean("sampleBean");
+		System.out.println(sampleBean);
+	}
+}
