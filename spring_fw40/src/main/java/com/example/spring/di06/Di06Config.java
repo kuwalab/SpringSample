34efdebfdb1ff1c00222e908e06d69c5c89bac08
kuwalab <arata.shinichi@gmail.com>
2014-05-04 22:15:31 +0900

    prototypeを定数に

--- spring_fw40/src/main/java/com/example/spring/di06/Di06Config.java
+++ spring_fw40/src/main/java/com/example/spring/di06/Di06Config.java
@@ -1,5 +1,6 @@
 package com.example.spring.di06;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -7,7 +8,7 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class Di06Config {
 	@Bean
-	@Scope("prototype")
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 	public Car car() {
 		return new Car("軽自動車");
 	}
